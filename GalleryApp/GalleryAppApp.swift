b4fe52274efa0477b98544fc2740d0360750d1b7
Shivanand <kolishivanand4@gmail.com>
2026-02-07 13:58:08 +0530

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Shivanand Koli on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
